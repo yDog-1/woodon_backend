fc237f2de6cf13cd4049041da43f5ecedd23a4c4
yDog <ydog-1@outlook.jp>
2024-09-06 13:22:14 +0000

    feat: コメント関連テーブル追加

---
+++ migrations/postgres/20240906125607-comments.sql
@@ -0,0 +1,12 @@
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS post_comments (
+  comment_id SERIAL PRIMARY KEY,
+  post_id INTEGER NOT NULL REFERENCES posts(post_id) ON DELETE CASCADE,
+  user_id INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+  content TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX idx_post_comments_post_id ON post_comments(post_id);
+CREATE INDEX idx_post_comments_user_id ON post_comments(user_id);
+-- +migrate Down
+DROP TABLE IF EXISTS post_comments;
